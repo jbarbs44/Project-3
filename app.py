206c52b2fc64962965fd812b390e80f4b597d430
Jason Barbagallo <jasonbarbagallo@gmail.com>
2023-06-06 20:00:31 -0400

    Updated App

--- app.py
+++ app.py
@@ -18,3 +18,47 @@ engine = create_engine("sqlite:///nobel_prize_winners.db")
 Base = automap_base()
 # reflect the tables
 Base.prepare(autoload_with=engine)
+
+# Save reference to the table
+Winners = Base.classes.nobel_winners
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return(
+        f"Available routes: <br/>"
+        f"/api/v1.0/Year<br/>"
+        f"/api/v1.0/Firstname<br/>"
+        f"/api/v1.0/Lastname<br/>"
+        f"/api/v1.0/Category<br/>"
+        f"/api/v1.0/Gender<br/>"
+        f"/api/v1.0/Motivation<br/>"
+        f"/api/v1.0/Birth_Date<br/>"
+        f"/api/v1.0/Birth_Country<br/>"
+        f"/api/v1.0/Death_Date<br/>"
+    )
+
+@app.route("/api/v1.0/Year<br/>")
+def Year():
+    #Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    #Query all years
+    results = session.query(Winners.Year).all()
+
+    session.close()
+
+    return jsonify(results)
+
+
+
+
